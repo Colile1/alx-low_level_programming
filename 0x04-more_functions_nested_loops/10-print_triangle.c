1620052442f4c54bf49fbcf16c25753ed39d5b73
Colile1 <colilesibanda@gmail.com>
2023-10-12 15:22:00 +0200

    10.2

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,27 +1,33 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a triangle
+ * print_triangle - prints a right-angled triangle
  * @size: size of the triangle
  */
+
 void print_triangle(int size)
 {
     if (size <= 0)
     {
         _putchar('\n');
-        return;
     }
-
-    int i, j;
-
-    for (i = 1; i <= size; i++)
+    else
     {
-        for (j = 1; j <= size - i; j++)
-            _putchar(' ');
+        int i, j;
 
-        for (j = 1; j <= i; j++)
-            _putchar('#');
+        for (i = 1; i <= size; i++)
+        {
+            for (j = i; j < size; j++)
+            {
+                _putchar(' ');
+            }
 
-        _putchar('\n');
+            for (j = 1; j <= i; j++)
+            {
+                _putchar('#');
+            }
+
+            _putchar('\n');
+        }
     }
 }
