df9563c1d44e4834b2d9998d37e782e07d24380b
Colile1 <colilesibanda@gmail.com>
2023-10-16 19:50:46 +0200

    kj

--- 0x05-pointers_arrays_strings/10-atoi.c
+++ 0x05-pointers_arrays_strings/10-atoi.c
@@ -1,25 +1,37 @@
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: A pointer to a string.
+ *
+ * Description: This function takes a pointer to a string as a parameter
+ * and converts the string to an integer. It takes into account negative
+ * and positive signs and returns the resulting integer.
+ * If there are no numbers in the string, it returns 0.
+ * Return: The converted integer.
+ */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int i = 0;
+int sign = 1;
+int result = 0;
+int i = 0;
 
-    while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
-        i++;
+if (s[0] == '-')
+{
+sign = -1;
+i = 1;
+}
 
-    if (s[i] == '-')
-    {
-        sign = -1;
-        i++;
-    }
-    else if (s[i] == '+')
-        i++;
+while (s[i] != '\0')
+{
+if (s[i] >= '0' && s[i] <= '9')
+{
+result = result * 10 + (s[i] - '0');
+}
+else
+{
+break;
+}
+i++;
+}
 
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
-
-    return result * sign;
+return sign * result;
 }
