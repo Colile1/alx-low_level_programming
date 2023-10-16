6b11920409c791656fb1dd7d8345ad8be0d8b062
Colile1 <colilesibanda@gmail.com>
2023-10-16 18:54:55 +0200

    gf

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,9 +7,10 @@
  */
 int _strlen(char *s)
 {
-    int len = 0;
-    while (s[len] != '\0') {
-        len++;
-    }
-    return len;
+int len = 0;
+while (s[len] != '\0') 
+{
+len++;
+}
+return len;
 }
