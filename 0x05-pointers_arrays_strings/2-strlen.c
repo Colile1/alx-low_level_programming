7894c3a0e2fedc0ca283d6522eb78c8e9c413fc3
Colile1 <colilesibanda@gmail.com>
2023-10-16 19:21:37 +0200

    jh

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,15 @@
  * @s: string
  * Return: length
  */
-int _strlen(char *s) /*strlen - Returns the length of a string*/
+int _strlen(char *s)
 {
-int len = 0;
-while (s[len] != '\0')
-{
-len++;
-}
-return (len);
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
 }
