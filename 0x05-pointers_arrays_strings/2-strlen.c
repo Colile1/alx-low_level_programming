e9278e2d05dfaa9039f365c7eacb57cd8700c93c
Colile1 <colilesibanda@gmail.com>
2023-10-16 19:00:57 +0200

    ef

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,11 @@
 /**
- * @_strlen: Returns the length of a string.
+ * strlen - Returns the length of a string.
  * @s: A pointer to a string.
  *
  * Description: This function takes a pointer to a string as a parameter
  * and returns the length of the string.
  */
-int _strlen(char *s)
+int _strlen(char *s) //strlen - Returns the length of a string
 {
 int len = 0;
 while (s[len] != '\0')
