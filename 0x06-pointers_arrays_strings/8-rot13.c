cd41dc84122b178723d18a529b93dcc0be3f39d2
Colile1 <colilesibanda@gmail.com>
2023-10-19 20:29:14 +0200

    q8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,21 +7,21 @@
  */
 char *rot13(char *s)
 {
-	int i, j;
-	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; input[j] != '\0'; j++)
+		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == input[j])
+			if (s[i] == data1[j])
 			{
-				s[i] = output[j];
+				s[i] = datarot[j];
 				break;
 			}
 		}
 	}
-
 	return (s);
 }
