1e066771a51569e131d0b11f31981e415fdcdba5
Colile1 <colilesibanda@gmail.com>
2023-10-19 15:54:04 +0200

    m

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,9 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 	int dest_len = 0;
 	int i;
 
+	/* Calculate the length of dest */
 	while (dest[dest_len] != '\0')
 		dest_len++;
 
+	/* Concatenate src to dest */
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_len + i] = src[i];
 
