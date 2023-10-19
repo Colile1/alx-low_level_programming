12db2883ee210cba8d3a51e5655777b1ff2ba149
Colile1 <colilesibanda@gmail.com>
2023-10-19 15:35:05 +0200

    Q3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: difference between the first non-matching characters of s1 and s2,
+ * or 0 if the strings are identical
+ */
+int _strcmp(char *s1, char *s2)
+{
+    int i = 0;
+
+    while (s1[i] != '\0' && s2[i] != '\0')
+    {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
+        i++;
+    }
+    return (s1[i] - s2[i]);
+}
