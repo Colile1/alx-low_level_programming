465d41d85d8e4c8dc0495ff379765ab624be2001
Colile1 <colilesibanda@gmail.com>
2023-10-26 00:39:51 +0200

    v

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,21 +10,12 @@ int is_prime_number(int n)
 {
     if (n <= 1)
         return (0);
-    if (n == 2)
-        return (1);
-    if (n % 2 == 0)
-        return (0);
-    if (n <= 3)
-        return (1);
-    if (n % 3 == 0)
-        return (0);
-    if (n % 5 == 0)
-        return (0);
-    if (n % 7 == 0)
-        return (0);
-    if (n <= 31)
-        return (1);
-    if (n % 31 == 0)
-        return (0);
-    return (is_prime_number(n - 1));
+    
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return (0);
+    }
+
+    return (1);
 }
