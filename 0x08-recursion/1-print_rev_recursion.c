3e5be1def9ca73e61519b4cdd3e927577ba310d0
Colile1 <colilesibanda@gmail.com>
2023-10-25 23:06:02 +0200

    hj

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,7 +9,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev_recursion(s + 1); // Recursively move to the end of the string
-		putchar(*s); // Print the character
+		_print_rev_recursion(s + 1);
+		putchar(*s);
 	}
 }
