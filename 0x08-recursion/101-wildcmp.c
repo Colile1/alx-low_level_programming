eba729f89201527d7359aa90564d9fb5a14c6ab9
Colile1 <colilesibanda@gmail.com>
2023-10-25 22:43:21 +0200

    et

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -36,3 +36,23 @@ int main(void)
     printf("%d\n", r);
     return (0);
 }
+
+/**
+ * wildcmp - Compares two strings with wildcards.
+ * @s1: The first string.
+ * @s2: The second string with wildcards.
+ *
+ * Return: 1 if identical, 0 if not.
+ */
+int wildcmp(char *s1, char *s2)
+{
+    if (*s2 == '*')
+    {
+        if (*(s2 + 1) == '*')
+            return (wildcmp(s1, s2 + 1));
+        return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+    }
+    if (*s1 == *s2)
+        return (*s1 == '\0' ? 1 : wildcmp(s1 + 1, s2 + 1));
+    return (0);
+}
