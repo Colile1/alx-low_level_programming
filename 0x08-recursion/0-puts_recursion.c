a1ba1aedc644add67b41a87ce4477c6a2befad32
Colile1 <colilesibanda@gmail.com>
2023-10-25 19:32:41 +0200

    sd

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -20,7 +20,7 @@ void _puts_recursion(char *s)
 {
     if (*s == '\0')
     {
-        putchar('\n'); // Print newline after the string is fully printed.
+        putchar('\n'); /* Print newline after the string is fully printed. */
         return;
     }
 
