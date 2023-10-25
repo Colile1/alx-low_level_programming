28331d3a90c1d2ff09f207d991d26aebb0f1897d
Colile1 <colilesibanda@gmail.com>
2023-10-25 23:32:30 +0200

    q5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * _pow_recursion - Returns the value of x raised to the power of y.
+ * @x: The base value.
+ * @y: The exponent.
  *
- * Return: Always 0.
+ * Return: The result of x^y. If y is lower than 0, returns -1.
  */
-int main(void)
+int _pow_recursion(int x, int y)
 {
-    int r;
-
-    r = _pow_recursion(1, 10);
-    printf("%d\n", r);
-    r = _pow_recursion(1024, 0);
-    printf("%d\n", r);
-    r = _pow_recursion(2, 16);
-    printf("%d\n", r);
-    r = _pow_recursion(5, 2);
-    printf("%d\n", r);
-    r = _pow_recursion(5, -2);
-    printf("%d\n", r);
-    r = _pow_recursion(-5, 3);
-    printf("%d\n", r);
-    return (0);
+	if (y < 0)
+	{
+		return (-1);
+	}
+	
+	if (y == 0)
+	{
+		return (1);
+	}
+	
+	return (x * _pow_recursion(x, y - 1));
 }
