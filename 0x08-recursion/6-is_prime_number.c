234935998e7e1b789c3ca1d596deee0d7685687f
Colile1 <colilesibanda@gmail.com>
2023-10-26 00:05:02 +0200

    xf

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,18 +4,24 @@
  * is_prime_number - Checks if a number is prime.
  * @n: The number to check.
  *
- * Return: 1 if prime, 0 if not.
+ * Return: 1 if the number is prime, 0 otherwise.
  */
 int is_prime_number(int n)
 {
     if (n <= 1)
         return (0);
-    if (n <= 3)
+
+    if (n == 2)
         return (1);
-    if (n % 2 == 0 || n % 3 == 0)
+
+    if (n % 2 == 0)
         return (0);
-    for (int i = 5; i * i <= n; i += 6)
-        if (n % i == 0 || n % (i + 2) == 0)
+
+    for (int i = 3; i * i <= n; i += 2)
+    {
+        if (n % i == 0)
             return (0);
+    }
+
     return (1);
 }
