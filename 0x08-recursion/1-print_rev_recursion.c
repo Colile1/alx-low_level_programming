da13c04556da9a6000d85682d2cd36f6e8840331
Colile1 <colilesibanda@gmail.com>
2023-10-25 22:50:58 +0200

    sdf

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,21 +8,19 @@
  */
 int main(void)
 {
-    _print_rev_recursion("Colton Walker\n");
+    _print_rev_recursion("\nColton Walker");
     return (0);
 }
 
-#include <stdio.h>
-
 /**
  * _print_rev_recursion - Prints a string in reverse.
- * @s: Pointer to the string to be printed in reverse.
+ * @s: The string to be printed in reverse.
  */
 void _print_rev_recursion(char *s)
 {
     if (*s)
     {
         _print_rev_recursion(s + 1);
-        putchar(*s);
+        _putchar(*s);
     }
 }
