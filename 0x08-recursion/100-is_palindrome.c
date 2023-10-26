498291f4c77dbd634bf4dc2c1a783f8896073868
Colile1 <colilesibanda@gmail.com>
2023-10-26 05:02:38 +0200

    qw

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,13 +12,13 @@
  */
 int is_palindrome_recursive(char *s, int start, int end)
 {
-    if (start >= end)
-        return 1;
+	if (start >= end)
+		return (1);
 
-    if (s[start] == s[end])
-        return is_palindrome_recursive(s, start + 1, end - 1);
+	if (s[start] == s[end])
+		return (is_palindrome_recursive(s, start + 1, end - 1));
 
-    return 0;
+	return (0);
 }
 
 /**
@@ -30,7 +30,7 @@ int is_palindrome_recursive(char *s, int start, int end)
  */
 int is_palindrome(char *s)
 {
-    int len = strlen(s);
+	int len = strlen(s);
 
-    return is_palindrome_recursive(s, 0, len - 1);
+	return (is_palindrome_recursive(s, 0, len - 1));
 }
