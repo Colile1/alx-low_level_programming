094408f328c0afb53934a09721438ee5a6d4c267
Colile1 <colilesibanda@gmail.com>
2023-10-31 12:30:51 +0200

    g

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,16 +10,16 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    char *arr;
-    unsigned int i;
+char *arr;
+unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+if (size == 0)
+return (NULL);
 
-    arr = (char *)malloc(size * sizeof(char));
+arr = (char *)malloc(size * sizeof(char));
 
-    if (arr == NULL)
-        return (NULL);
+if (arr == NULL)
+return (NULL);
 
     for (i = 0; i < size; i++)
         arr[i] = c;
