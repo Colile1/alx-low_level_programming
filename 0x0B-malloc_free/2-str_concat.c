11d31c84293535ef39e8d0c1a90cc5cd7aaa10e4
Colile1 <colilesibanda@gmail.com>
2023-10-31 12:46:23 +0200

    t2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,28 +10,28 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *concatenated;
-    unsigned int len1, len2, i, j;
+	char *concatenated;
+	unsigned int len1, len2, i, j;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    for (len1 = 0; s1[len1]; len1++)
-        ;
-    for (len2 = 0; s2[len2]; len2++)
-        ;
+	for (len1 = 0; s1[len1]; len1++)
+		;
+	for (len2 = 0; s2[len2]; len2++)
+		;
 
-    concatenated = (char *)malloc((len1 + len2 + 1) * sizeof(char));
-    if (concatenated == NULL)
-        return (NULL);
+	concatenated = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	if (concatenated == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        concatenated[i] = s1[i];
-    for (j = 0; j < len2; j++, i++)
-        concatenated[i] = s2[j];
+	for (i = 0; i < len1; i++)
+		concatenated[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		concatenated[i] = s2[j];
 
-    concatenated[i] = '\0';
-    return (concatenated);
+	concatenated[i] = '\0';
+	return (concatenated);
 }
