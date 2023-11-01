d59bdff66a0027601492f20129910f6293d9a1e8
Colile1 <colilesibanda@gmail.com>
2023-11-01 18:42:07 +0200

    0x

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program.
+ * @argc: The number of arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[]) {
+    if (argc > 0) {
+        printf("%s\n", argv[0]);
+        return (0);
+    } else {
+        return (1);
+    }
+}
