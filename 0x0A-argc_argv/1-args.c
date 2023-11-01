198b4236b35b2de8d7fa1806ab0c5a46b4b620ff
Colile1 <colilesibanda@gmail.com>
2023-11-02 00:42:50 +0200

    l

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * @argc: The number of command-line arguments
- * @argv: An array containing the program's command-line arguments
- *
- * Return: 0
- */
+* main - Entry point
+* @argc: The number of command-line arguments
+* @argv: An array containing the program's command-line arguments
+*
+* Return: 0
+*/
 int main(int argc, char *argv[])
 {
-    printf("%d\n", argc - 1);
+printf("%d\n", argc - 1);
 (void)argv;
 
 return (0);
