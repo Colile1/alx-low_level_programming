ee406877b20348213e46cbb61cb37f6061258865
Colile1 <colilesibanda@gmail.com>
2023-11-01 22:35:52 +0200

    jh

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+    printf("%d\n", argc - 1);
+    return (0);
+}
