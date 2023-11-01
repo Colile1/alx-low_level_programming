f4ca5b80a05a8c18bae05985f60e669e7bbd3851
Colile1 <colilesibanda@gmail.com>
2023-11-02 00:28:56 +0200

    b6

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,17 +5,12 @@
  * @argc: The number of command-line arguments
  * @argv: An array containing the program's command-line arguments
  *
- * Return: 0 if successful, 1 if no arguments were passed
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-    if (argc > 0)
-    {
-        printf("%d\n", argc - 1);
-        return (0);
-    }
-    else
-    {
-        return (1);
-    }
+    printf("%d\n", argc - 1);
+    (void)argv;
+
+    return (0);
 }
