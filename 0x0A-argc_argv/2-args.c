c7927fc4bd353ac587dabc38ceb27e1d3783854c
Colile1 <colilesibanda@gmail.com>
2023-11-02 01:13:19 +0200

    ib

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,12 +6,15 @@
  * @argv: array of arguments
  * Return: 0 if successful
  */
+
 int main(int argc, char *argv[])
 {
     int i;
+
     for (i = 0; i < argc; i++)
     {
         printf("%s\n", argv[i]);
     }
+
     return (0);
 }
