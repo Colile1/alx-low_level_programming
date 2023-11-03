14fbb050036615a989ac98a140ea8210bc9a8cfb
Colile1 <colilesibanda@gmail.com>
2023-11-03 03:02:05 +0200

    df

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,14 +8,11 @@
 * Return: A pointer to the allocated memory
 */
 
-void *malloc_checked(unsigned int b) 
-{
- void *ptr = malloc(b);
- if (ptr == NULL)
- {
-   fprintf(stderr, "Memory allocation failed\n");
-   exit(98);
- }
-
- return ptr;
+void *malloc_checked(unsigned int b) {
+    void *ptr = malloc(b);
+    if (ptr == NULL) {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(98);
+    }
+    return ptr;
 }
