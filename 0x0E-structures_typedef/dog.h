1cb08d382709f1567582f55a7a3804517faa82d7
Colile1 <colilesibanda@gmail.com>
2023-11-06 16:14:19 +0200

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,20 @@
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - Structure representing a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: Structure representing a dog including its name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
+/**
+ * dog_t - Typedef for struct dog
+ */
 typedef struct dog dog_t;
 
