739ed6844a17c4d954a9a3ba1549b2ac26530267
Colile1 <colilesibanda@gmail.com>
2023-11-09 23:22:21 +0200

    a2

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,44 @@
+/* 100-main_opcodes.c */
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * @argc: Number of arguments
- * @argv: Array of arguments
- * Return: 0 on success, error code on failure
+ * main - Prints the opcodes of its own main function.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ *
+ * Return: 0 on success, 1 for incorrect number of arguments,
+ *         2 for negative number of bytes.
  */
 int main(int argc, char *argv[])
 {
-    int num_bytes, i;
-    unsigned char *opcodes;
+int num_bytes, i;
+char *main_opcode;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-    num_bytes = atoi(argv[1]);
+num_bytes = atoi(argv[1]);
 
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        return 2;
-    }
+if (num_bytes < 0)
+{
+printf("Error\n");
+return (2);
+}
 
-    opcodes = (unsigned char *)main;
+main_opcode = (char *)main;
 
-    for (i = 0; i < num_bytes; i++)
-    {
-        printf("%02x ", opcodes[i]);
-    }
+for (i = 0; i < num_bytes; i++)
+{
+printf("%02hhx", main_opcode[i]);
+if (i < num_bytes - 1)
+printf(" ");
+else
+printf("\n");
+}
 
-    printf("\n");
-
-    return 0;
+return (0);
 }
