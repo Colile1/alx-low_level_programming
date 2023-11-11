526d0f22aca99c6195e3d3d1d5d3790796c1c2ec
Colile1 <colilesibanda@gmail.com>
2023-11-11 16:19:46 +0200

    a1

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,35 +2,47 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * @argc: The number of command-line arguments
- * @argv: An array containing the program's command-line arguments
+ * main - Print the minimum number of coins to make change for an amount.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
  *
- * Return: 0 if successful, 1 if invalid arguments
+ * Return: 0 on success, 1 on error.
  */
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         printf("Error\n");
         return 1;
     }
 
     int cents = atoi(argv[1]);
-    if (cents < 0) {
+
+    if (cents < 0)
+    {
         printf("0\n");
         return 0;
     }
 
-    int quarters = cents / 25;
-    cents %= 25;
-    int dimes = cents / 10;
-    cents %= 10;
-    int nickels = cents / 5;
-    cents %= 5;
-    int pennies = cents;
+    int coins = 0;
 
-    int totalCoins = quarters + dimes + nickels + pennies;
-    printf("%d\n", totalCoins);
+    while (cents > 0)
+    {
+        if (cents >= 25)
+            cents -= 25;
+        else if (cents >= 10)
+            cents -= 10;
+        else if (cents >= 5)
+            cents -= 5;
+        else if (cents >= 2)
+            cents -= 2;
+        else
+            cents -= 1;
+
+        coins++;
+    }
+
+    printf("%d\n", coins);
 
     return 0;
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,32 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        printf("0\n");
-        return 0;
-    }
-
-    int sum = 0;
-    int error = 0;
-
-    for (int i = 1; i < argc; i++) {
-        char *end;
-        long number = strtol(argv[i], &end, 10);
-
-        if (number <= 0 || *end != '\0') {
-            error = 1;
-            break;
-        }
-
-        sum += number;
-    }
-
-    if (error) {
+/**
+ * main - Multiply two numbers.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
         printf("Error\n");
         return 1;
     }
 
-    printf("%d\n", sum);
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+
+    printf("%d\n", num1 * num2);
+
     return 0;
 }
