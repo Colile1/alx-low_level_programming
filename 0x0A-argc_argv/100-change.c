04a366a96882db05f9b6616538f5ecb9d2c12176
Colile1 <colilesibanda@gmail.com>
2023-11-11 16:28:04 +0200

    a2

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,48 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * main - Print the minimum number of coins to make change for an amount.
- * @argc: Number of arguments.
- * @argv: Array of arguments.
- *
- * Return: 0 on success, 1 on error.
- */
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         printf("Error\n");
         return 1;
     }
 
     int cents = atoi(argv[1]);
 
-    if (cents < 0)
-    {
+    if (cents < 0) {
         printf("0\n");
         return 0;
     }
 
-    int coins = 0;
+    int coins[] = {25, 10, 5, 2, 1};
+    int numCoins = 0;
 
-    while (cents > 0)
-    {
-        if (cents >= 25)
-            cents -= 25;
-        else if (cents >= 10)
-            cents -= 10;
-        else if (cents >= 5)
-            cents -= 5;
-        else if (cents >= 2)
-            cents -= 2;
-        else
-            cents -= 1;
-
-        coins++;
+    for (int i = 0; i < 5; i++) {
+        while (cents >= coins[i]) {
+            cents -= coins[i];
+            numCoins++;
+        }
     }
 
-    printf("%d\n", coins);
-
+    printf("%d\n", numCoins);
     return 0;
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * main - Multiply two numbers.
- * @argc: Number of arguments.
- * @argv: Array of arguments.
- *
- * Return: 0 on success, 1 on error.
- */
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return 1;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("0\n");
+        return 0;
     }
 
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
+    int sum = 0;
+    for (int i = 1; i < argc; i++) {
+        char *endptr;
+        long num = strtol(argv[i], &endptr, 10);
 
-    printf("%d\n", num1 * num2);
+        if (*endptr != '\0') {
+            printf("Error\n");
+            return 1;
+        }
 
+        sum += num;
+    }
+
+    printf("%d\n", sum);
     return 0;
 }
