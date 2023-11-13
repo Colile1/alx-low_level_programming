5ef284868958729258df4a937392e546b68699ed
Colile1 <colilesibanda@gmail.com>
2023-11-13 12:28:13 +0200

    0x0A-argc_argv 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,13 +12,14 @@
  */
 int main(int argc, char *argv[])
 {
+    int sum = 0;
+
     if (argc < 2)
     {
         printf("0\n");
-        return 0;
+        return (0);
     }
 
-    int sum = 0;
     for (int i = 1; i < argc; i++)
     {
         char *endptr;
@@ -27,12 +28,12 @@ int main(int argc, char *argv[])
         if (*endptr != '\0')
         {
             printf("Error\n");
-            return 1;
+            return (1);
         }
 
         sum += num;
     }
 
     printf("%d\n", sum);
-    return 0;
+    return (0);
 }
