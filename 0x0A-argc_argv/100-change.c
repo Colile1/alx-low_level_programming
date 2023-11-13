16b32c2014f546a471f59e085df2029ffafb6e95
Colile1 <colilesibanda@gmail.com>
2023-11-13 12:49:19 +0200

    0x0A-argc_argv 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,29 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the program's command-line arguments
+ *
+ * Return: 0 if successful, 1 if there is an error
+ */
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
+int cents;
+int coins[] = {25, 10, 5, 2, 1};
+int numCoins = 0;
+int i;
 
-    int cents = atoi(argv[1]);
+if (argc != 2) {
+printf("Error\n");
+return (1);
+}
 
-    if (cents < 0) {
-        printf("0\n");
-        return 0;
-    }
+cents = atoi(argv[1]);
 
-    int coins[] = {25, 10, 5, 2, 1};
-    int numCoins = 0;
+if (cents < 0) {
+printf("0\n");
+return (0);
+}
 
-    for (int i = 0; i < 5; i++) {
-        while (cents >= coins[i]) {
-            cents -= coins[i];
-            numCoins++;
-        }
-    }
+for (i = 0; i < 5; i++) {
+while (cents >= coins[i]) {
+cents -= coins[i];
+numCoins++;
+}
+}
 
-    printf("%d\n", numCoins);
-    return 0;
+printf("%d\n", numCoins);
+return (0);
 }
