029ae3578a1b49203dbe21620876ecace9e62e13
Colile1 <colilesibanda@gmail.com>
2023-11-16 18:43:39 +0200

    modified:   0x12-singly_linked_lists/100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
+void print_before_main(void) __attribute__((constructor));
+
 /**
  * print_before_main - prints a message before main function is executed
  */
-void print_before_main(void) __attribute__((constructor));
-
 void print_before_main(void)
 {
-printf("You're beat! and yet, you must allow,\n
-I bore my house upon my back!\n");
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
+
+int main(void)
+{
+    /* main function code */
+    return 0;
+}
+
