51c067671fb6cbcaa1c91a8dc0da476f1ed2470a
Colile1 <colilesibanda@gmail.com>
2023-11-22 00:24:46 +0200

    qqq

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,40 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely
- * @head: Pointer to the head of the list
- *
- * Description: Number of nodes in the list
- */
-
-/**
- * print_listint - Prints a listint_t linked list
+ * print_listint_safe - Prints a listint_t linked list
  * @head: Pointer to the head of the list
  *
  * Return: The number of nodes in the list
  */
-
-size_t print_listint(const listint_t *head)
+size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *current = head;
+    const listint_t *slow = head;
+    const listint_t *fast = head;
     size_t count = 0;
 
-    while (current)
+    if (head == NULL)
+        exit(98);
+
+    while (slow && fast && fast->next)
     {
-        printf("%d\n", current->n);
+        printf("[%p] %d\n", (void *)slow, slow->n);
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            printf("Loop found, exiting...\n");
+            exit(98);
+        }
+
         count++;
-        current = current->next;
     }
 
-    return count;
-}
+    printf("[%p] %d\n", (void *)slow, slow->n);
+    count++;
 
+    return (count);
+}
