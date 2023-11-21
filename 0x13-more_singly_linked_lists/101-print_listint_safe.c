2c05687da09a58ea42cc1b9bb4045928f3c9bb0b
Colile1 <colilesibanda@gmail.com>
2023-11-21 22:56:40 +0200

    b2

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,7 +6,7 @@
  * print_listint_safe - Prints a listint_t linked list safely
  * @head: Pointer to the head of the list
  *
- * Return: Number of nodes in the list
+ * Description: Number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
