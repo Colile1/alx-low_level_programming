d8f953b9f63911fe4425ef12428dd0f5fb58faa1
Colile1 <colilesibanda@gmail.com>
2023-11-21 21:29:10 +0200

    modified:   0x13-more_singly_linked_lists/101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,33 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
-/**
- * print_listint_safe - Prints a listint_t linked list
- * @head: Pointer to the head of the list
- *
- * Return: The number of nodes in the list
- */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *current = head;
-size_t count = 0;
+    const listint_t *current = head;
+    size_t count = 0;
+    const listint_t *loop_node = NULL;
 
-while (current != NULL)
-{
-printf("[%p] %d\n", (void *)current, current->n);
-count++;
-if (current <= current->next)
-{
-printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-break;
-}
-current = current->next;
-}
+    while (current != NULL)
+    {
+        if (current == loop_node)
+            break;
 
-return (count);
+        printf("[%p] %d\n", (void *)current, current->n);
+        count++;
+
+        if (current->next == loop_node)
+            break;
+
+        current = current->next;
+
+        if (loop_node == NULL && current != NULL && current->next != NULL)
+            loop_node = current->next;
+    }
+
+    if (loop_node != NULL)
+        printf("-> [%p] %d\n", (void *)loop_node, loop_node->n);
+
+    return (count);
 }
