4758264acca3eff21b2d8642baca6d80c3345f7d
Colile1 <colilesibanda@gmail.com>
2023-11-21 22:34:45 +0200

    d3

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,36 +1,28 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t linked list
+ * free_listint_safe - Frees a listint_t list safely
  * @h: Double pointer to the head of the list
  *
  * Return: The number of nodes freed
  */
 size_t free_listint_safe(listint_t **h)
 {
+    listint_t *tmp;
+    listint_t *current;
     size_t count = 0;
-    listint_t *current, *next;
 
-    if (h == NULL || *h == NULL)
-        return (count);
+    if (!h || !*h)
+        return (0);
 
     current = *h;
-    while (current != NULL)
+    while (current)
     {
-        next = current->next;
+        tmp = current->next;
         free(current);
+        current = tmp;
         count++;
-
-        if (current <= next)
-        {
-            printf("Loop detected, freeing stopped\n");
-            break;
-        }
-
-        current = next;
     }
 
     *h = NULL;
