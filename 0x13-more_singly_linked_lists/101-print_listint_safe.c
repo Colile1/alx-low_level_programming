f17248ed466baeeeca87bc4498134c45e9b8bff5
Colile1 <colilesibanda@gmail.com>
2023-11-21 22:43:09 +0200

    d7

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,47 +3,30 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list
+ * print_listint_safe - Prints a listint_t linked list safely
  * @head: Pointer to the head of the list
  *
- * Return: The number of nodes in the list
+ * Return: Number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    const listint_t *slow, *fast;
+    const listint_t *current = head;
     size_t count = 0;
 
-    if (head == NULL)
-        exit(98);
-
-    slow = head;
-    fast = head;
-
-    while (fast != NULL && fast->next != NULL)
+    while (current)
     {
-        printf("[%p] %d\n", (void *)slow, slow->n);
+        /* Print the current node */
+        printf("[%p] %d\n", (void *)current, current->n);
         count++;
 
-        slow = slow->next;
-        fast = fast->next->next;
-
-        if (slow == fast)
+        if (current <= current->next)
         {
-            printf("-> [%p] %d\n", (void *)slow, slow->n);
+            /* Print the looped node and exit the loop */
+            printf("-> [%p] %d\n", (void *)current->next, current->next->n);
             break;
         }
-    }
 
-    while (fast != NULL && fast->next != NULL)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-
-        if (slow == fast)
-        {
-            printf("-> [%p] %d\n", (void *)slow, slow->n);
-            break;
-        }
+        current = current->next;
     }
 
     return (count);
