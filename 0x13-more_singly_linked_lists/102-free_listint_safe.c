2981e65dd9bf0001eed8243ae228a93a6a4be822
Colile1 <colilesibanda@gmail.com>
2023-11-21 23:16:15 +0200

    q2

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,60 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a listint_t list safely
- * @h: Double pointer to the head of the list
- *
- * Return: The number of nodes freed
+ * free_listint_safe - frees a listint_t list
+ * @h: pointer to the head of the list
+ * Return: the size of the list that was freed
  */
 size_t free_listint_safe(listint_t **h)
 {
-    listint_t *tmp;
-    listint_t *current;
     size_t count = 0;
+    listint_t *slow, *fast, *loop, *tmp;
 
-    if (!h || !*h)
+    if (h == NULL || *h == NULL)
         return (0);
 
-    current = *h;
-    while (current)
+    slow = fast = *h;
+    /* detect loop using Floyd's cycle-finding algorithm */
+    while (slow && fast && fast->next)
     {
-        tmp = current->next;
-        free(current);
-        current = tmp;
-        count++;
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) /* loop found */
+        {
+            loop = slow;
+            break;
+        }
     }
-
-    *h = NULL;
+    /* if no loop, free the list normally */
+    if (loop == NULL)
+    {
+        while (*h)
+        {
+            tmp = *h;
+            *h = (*h)->next;
+            free(tmp);
+            count++;
+        }
+    }
+    else /* if loop, free the list until loop point */
+    {
+        while (*h != loop)
+        {
+            tmp = *h;
+            *h = (*h)->next;
+            free(tmp);
+            count++;
+        }
+        /* free the loop node and the next node */
+        tmp = *h;
+        *h = (*h)->next;
+        free(tmp);
+        count++;
+        tmp = *h;
+        *h = NULL;
+        free(tmp);
+    }
     return (count);
 }
