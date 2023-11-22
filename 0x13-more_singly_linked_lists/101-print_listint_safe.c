2618f35f554d0527d916b3f62c868b2bec2994fc
Colile1 <colilesibanda@gmail.com>
2023-11-22 10:31:37 +0200

    modified:   0x13-more_singly_linked_lists/101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,60 +1,31 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list
- * @head: Pointer to the head of the list
+ * print_listint_safe - Prints a listint_t linked list safely
+ * @head: Pointer to the beginning of the linked list
  *
  * Return: The number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t count = 0;
     const listint_t *current = head;
-    const listint_t *loop_start = find_listint_loop((listint_t *)head);
-
-    while (current != NULL && current != loop_start)
-    {
-        printf("[%p] %d\n", (void *)current, current->n);
-        current = current->next;
-        count++;
-    }
-
-    if (current == loop_start)
-    {
-        printf("-> [%p] %d\n", (void *)current, current->n);
-        count++;
-    }
-
-    if (current != NULL)
-    {
-        printf("-> [%p] %d\n", (void *)current, current->n);
-        printf("Error: linked list contains a loop\n");
-        exit(98);
-    }
-
-    return count;
-}
-
-/**
- * print_listint - Prints a listint_t linked list
- * @head: Pointer to the head of the list
- *
- * Return: The number of nodes in the list
- */
-size_t print_listint(const listint_t *head)
-{
     size_t count = 0;
-    const listint_t *current = head;
 
     while (current != NULL)
     {
         printf("[%p] %d\n", (void *)current, current->n);
-        current = current->next;
         count++;
+
+        if (current <= current->next)
+        {
+            printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+            break;
+        }
+
+        current = current->next;
     }
 
     return count;
 }
-
