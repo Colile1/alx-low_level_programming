539b787b2e455494270df11a61aa4d721f10fccb
Colile1 <colilesibanda@gmail.com>
2023-11-23 12:20:49 +0200

    betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,15 +10,15 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    unsigned long int mask;
+unsigned long int mask;
 
-    if (index >= sizeof(unsigned long int) * 8)
-        return (-1);
+if (index >= sizeof(unsigned long int) * 8)
+return (-1);
 
-    mask = 1UL << index;
+mask = 1UL << index;
 
-    if (n & mask)
-        return (1);
-    else
-        return (0);
+if (n & mask)
+return (1);
+else
+return (0);
 }
