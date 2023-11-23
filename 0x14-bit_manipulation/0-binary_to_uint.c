d2e98bd72acc4aaaa84742c6cef084aecf58462d
Colile1 <colilesibanda@gmail.com>
2023-11-23 10:09:44 +0200

    new file:   0x14-bit_manipulation/0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: the binary number as a string
+ *
+ * Return: the converted unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int num = 0;
+int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+		i++;
+	}
+
+	return (num);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: the binary number as a string
+ *
+ * Return: the converted unsigned int
+ */
+unsigned int binary_to_uint(const char *b);
+
+#endif
