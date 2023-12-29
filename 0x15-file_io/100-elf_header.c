cb2672145fcb76325a915c26c2a353ff75ee386e
Colile1 <colilesibanda@gmail.com>
2023-12-29 22:03:27 +0200

    mn

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,10 +8,10 @@
 
 void elf_checker(unsigned char *e_ident);
 void magic_printer(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
+void class_printer(unsigned char *e_ident);
+void data_printer(unsigned char *e_ident);
+void version_printer(unsigned char *e_ident);
+void abi_printer(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
@@ -64,10 +64,10 @@ void magic_printer(unsigned char *e_ident)
 }
 
 /**
- * print_class - Prints the class of an ELF header.
+ * class_printer - Prints the class of an ELF header.
  * @e_ident: A pointer to an array containing the ELF class.
  */
-void print_class(unsigned char *e_ident)
+void class_printer(unsigned char *e_ident)
 {
 	printf("  Class:                             ");
 
@@ -88,10 +88,10 @@ void print_class(unsigned char *e_ident)
 }
 
 /**
- * print_data - Prints the data of an ELF header.
+ * data_printer - Prints the data of an ELF header.
  * @e_ident: A pointer to an array containing the ELF class.
  */
-void print_data(unsigned char *e_ident)
+void data_printer(unsigned char *e_ident)
 {
 	printf("  Data:                              ");
 
@@ -112,10 +112,10 @@ void print_data(unsigned char *e_ident)
 }
 
 /**
- * print_version - Prints the version of an ELF header.
+ * version_printer - Prints the version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF version.
  */
-void print_version(unsigned char *e_ident)
+void version_printer(unsigned char *e_ident)
 {
 	printf("  Version:                           %d",
 	       e_ident[EI_VERSION]);
@@ -177,10 +177,10 @@ void print_osabi(unsigned char *e_ident)
 }
 
 /**
- * print_abi - Prints the ABI version of an ELF header.
+ * abi_printer - Prints the ABI version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF ABI version.
  */
-void print_abi(unsigned char *e_ident)
+void abi_printer(unsigned char *e_ident)
 {
 	printf("  ABI Version:                       %d\n",
 	       e_ident[EI_ABIVERSION]);
@@ -300,11 +300,11 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	elf_checker(header->e_ident);
 	printf("ELF Header:\n");
 	magic_printer(header->e_ident);
-	print_class(header->e_ident);
-	print_data(header->e_ident);
-	print_version(header->e_ident);
+	class_printer(header->e_ident);
+	data_printer(header->e_ident);
+	version_printer(header->e_ident);
 	print_osabi(header->e_ident);
-	print_abi(header->e_ident);
+	abi_printer(header->e_ident);
 	print_type(header->e_type, header->e_ident);
 	print_entry(header->e_entry, header->e_ident);
 
