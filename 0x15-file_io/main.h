77bff518fea8cad7557df04596b045eb18b59567
Colile1 <colilesibanda@gmail.com>
2023-12-29 21:50:08 +0200

    modified:   0x15-file_io/100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,34 +1,26 @@
-#include <elf.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-#ifndef MAIN_H
-#define MAIN_H
-
-/* Prototypes*/
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int _putchar(char c);
-void my_printf(const char *format, ...);
-size_t get_line_length(const char *str);
-char *my_strcat(char *dest, const char *src);
 
-/*elf functions*/
-void elf_checker(unsigned char *elf_array_pointer);
-void elf_close(int elf);
-void elf_abi_print(unsigned char *elf_array_pointer);
-void elf_class_print(unsigned char *elf_array_pointer);
-void elf_h_data_printer(unsigned char *elf_array_pointer);
-void elf_entry_print(unsigned long int elf_entry, unsigned char *elf_array_pointer);
-void elf_magic_no_print(unsigned char *elf_array_pointer);
-void elf_osabi_print(unsigned char *elf_array_pointer);
-void elf_type_print(unsigned int elf_type, unsigned char *elf_array_pointer);
-void elf_version_print(unsigned char *elf_array_pointer);
+/*100*/
+void elf_checker(unsigned char *e_ident);
+void magic_printer(unsigned char *e_ident);
+void class_printer(unsigned char *e_ident);
+void data_printer(unsigned char *e_ident);
+void version_printer(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void osabi_printer(unsigned char *e_ident);
+void type_printer(unsigned int e_type, unsigned char *e_ident);
+void entry_printer(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
-#endif 
+#endif
