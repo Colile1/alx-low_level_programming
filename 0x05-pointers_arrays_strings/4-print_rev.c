c3d45a18852d246b2155c05aecce21a7c6ceefd1
colile1 <colilesibanda@gmail.com>
2025-07-18 11:14:27 +0200

    0x05-pointers_arrays_strings/4-print_rev.c Fixed variable name in print_rev function

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,23 +2,24 @@
 
 /**
  * print_rev - prints a string in reverse followed by a new line
- * @s: the string to print in reverse
+ * @s: the string to be printed in reverse
  */
 void print_rev(char *s)
 {
-	int length = 0;
+	int len = 0;
 	int i;
 
-	/* Calculate string length */
-	while (s[length] != '\0')
+	/* Find the length of the string */
+	while (s[len] != '\0')
 	{
-		length++;
+		len++;
 	}
 
-	/* Print characters in reverse order */
-	for (i = length - 1; i >= 0; i--)
+	/* Print the string in reverse */
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
+
 	_putchar('\n');
 }
